ff8ffdb0a0f156d1da715369c73183b0a8848611
Maxim Karpov <maxim.karpov@icloud.com>
2020-06-21 21:07:00 -0400

    fixed some issues, started the js

---
+++ SEG3125Lab5/main.js
@@ -0,0 +1,12 @@
+/*My code for checking the input fields
+*/
+
+function checkInputs{
+	var fName = document.getElementById("fname");
+	var lName = document.getElementById("lname");
+	var phoneNb = document.getElementById("phone");
+	var email = document.getElementById("email");
+	var creditC = document.getElementById("creditCard");
+
+	return
+}
